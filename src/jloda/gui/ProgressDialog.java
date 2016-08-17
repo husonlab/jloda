e0a3fdc01692333082d61ec08fdbd66a292fccf0
Daniel <Daniel.Huson@uni-tuebingen.de>
2016-08-17 16:27:52 +0800

    more work on problem of inspector window opening in the back

--- src/jloda/gui/ProgressDialog.java
+++ src/jloda/gui/ProgressDialog.java
@@ -457,7 +457,7 @@ public class ProgressDialog implements ProgressListener {
             run(new Runnable() {
                     public void run() {
                         if (owner != null && owner instanceof Window) {
-                            ((Window) owner).toFront();
+                            // ((Window) owner).toFront(); // this causes weird effects
                         }
                         if (progressBar != null) {
                             updateTaskLabel();
@@ -489,7 +489,7 @@ public class ProgressDialog implements ProgressListener {
     private static void run(Runnable runnable) {
         if (SwingUtilities.isEventDispatchThread())
             runnable.run();
-        else if (Platform.isFxApplicationThread())
+        else if (true || Platform.isFxApplicationThread())
             SwingUtilities.invokeLater(runnable);
         else // todo: this may lead to FX vs Swing deadlock. But not using this cases Inspector window to appear below current window
             try {
@@ -506,7 +506,6 @@ public class ProgressDialog implements ProgressListener {
     public static void setDelayInMilliseconds(long delayInMilliseconds) {
         ProgressDialog.delayInMilliseconds = delayInMilliseconds;
     }
-
 
     /**
      * in debug mode, report tasks and subtasks to stderr, too
