a0f16f821be7b30d9c32c3d6da64b8bdebeb632b
Noah Dietrich <52662093+njmdietrich@users.noreply.github.com>
2021-11-25 15:15:02 +0100

    changed package name

--- src/jloda/seq/FastAFileIterator.java
+++ src/jloda/seq/FastAFileIterator.java
@@ -17,9 +17,8 @@
  *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-package alora.io;
+package jloda.seq;
 
-import jloda.seq.FastQAsFastAFileIterator;
 import jloda.util.*;
 
 import java.io.*;
