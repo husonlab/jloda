27609c7197f0e23bdc93d47711dd7a20e6ffa931
Daniel <daniel.huson@uni-tuebingen.de>
2022-04-25 17:04:45 +0200

    more work on MSA viewer

--- src/jloda/util/ProgramProperties.java
+++ src/jloda/util/ProgramProperties.java
@@ -30,7 +30,6 @@ import java.awt.print.PageFormat;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.io.OutputStream;
 import java.util.*;
 import java.util.function.Function;
 
@@ -98,7 +97,7 @@ public class ProgramProperties {
      * load properties from default file
      */
     public static void load() {
-        try (FileInputStream fis = new FileInputStream(getDefaultFileName())) {
+        try (var fis = new FileInputStream(getDefaultFileName())) {
             props.load(fis);
             //System.err.println("Loaded properties from: " + getDefaultFileName());
         } catch (Exception ex) {
@@ -118,7 +117,7 @@ public class ProgramProperties {
      * save properties to default file
      */
     public static void store() {
-        try (OutputStream fos = new FileOutputStream(getDefaultFileName())) {
+        try (var fos = new FileOutputStream(getDefaultFileName())) {
             props.store(fos, programName);
             //System.err.println("Stored properties to: " + getDefaultFileName());
         } catch (Exception ex) {
