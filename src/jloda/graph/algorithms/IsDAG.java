d415a8f5fb3607a2c2e1b9124e2b1d9dc4b783fe
Daniel <daniel.huson@uni-tuebingen.de>
2023-10-09 12:10:30 +0200

    worked on support for frieze tilings

--- src/jloda/graph/algorithms/IsDAG.java
+++ src/jloda/graph/algorithms/IsDAG.java
@@ -1,5 +1,5 @@
 /*
- * IsDAG.java Copyright (C) 2022. Daniel H. Huson
+ * IsDAG.java Copyright (C) 2023. Daniel H. Huson
  *
  *  (Some files contain contributions from other authors, who are then mentioned separately.)
  *
